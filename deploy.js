6a5e72f88b9a399d15d4c299a8235a6e67991d4f
Ansh Kumar <anshkumar9686@gmail.com>
2024-11-16 18:34:00 +0530

    commit

---
+++ deploy.js
@@ -0,0 +1,7 @@
+const ethers = require("ethers");
+const fs 
+
+async function main(){
+    const provider = new ethers.providers.JsonRpcProvider("http://0.0.0.0:8545/");
+    const wallet = new ethers.Wallet("0xaf5a10b38ad3e3fa6efb03d514e161a65227fe174aa3d611f84140e1f1fef7cf", provider);
+}
