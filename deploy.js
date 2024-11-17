948a3ce9077a976bec48b72aae58b1e51f7ebc30
Ansh Kumar <anshkumar9686@gmail.com>
2024-11-17 10:31:51 +0530

    push

--- deploy.js
+++ deploy.js
@@ -12,3 +12,9 @@ async function main(){
     console.log(contract);
 }
 
+main()
+    .then(() => process.exit(0))
+    .catch((error) => {
+        console.error(error)
+        process.exit(1)
+    })
