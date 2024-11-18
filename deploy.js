965f4d5375d750453ea0d6cc786831fc9c4641ec
Ansh Kumar <anshkumar9686@gmail.com>
2024-11-18 19:42:48 +0530

    receipt

--- deploy.js
+++ deploy.js
@@ -3,13 +3,17 @@ const fs = require("fs-extra");
 
 async function main(){
     const provider = new ethers.providers.JsonRpcProvider("HTTP://127.0.0.1:7545");
-    const wallet = new ethers.Wallet("0x2ef7d6e3454757df132ed24d6c71d1b50dec4539f7263387b105ff2d8c42c8f9", provider);
+    const wallet = new ethers.Wallet("0x5758aeaab84a7289fccf5249b0a5212640f95c8eda89d74418462255ef6c0a85", provider);
     const abi=fs.readFileSync("./storage_sol_SimpleStorage.abi", "utf8");
     const binary = fs.readFileSync("./storage_sol_SimpleStorage.bin","utf8");
+
     const contractFactory = new ethers.ContractFactory(abi, binary, wallet);
     const contract =await contractFactory.deploy({gasPrice: 1000000000}); //wait for contract to deploy
     console.log("Deploying, please wait.....");
     console.log(contract);
+    const deploymentReceipt = await contract.deploymentTransaction.wait(1);
+    console.log("Here's the deployment receipt");
+    console.log(deploymentReceipt);
 }
 
 main()
